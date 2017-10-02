df0d05e97c7dc6d0150d80516d558a4b15667a4c
Toyouthat <32089189+toyouthat@users.noreply.github.com>
2017-10-02 08:52:00 +0200

    ok nav
    puesto enlace a estadísticas

--- view/section/nav.php
+++ view/section/nav.php
@@ -3,5 +3,6 @@
     <li><a href="<?php echo $BASE_URL; ?>/">Home</a></li>
     <li><a href="<?php echo $BASE_URL; ?>/temas">Preguntas por tema</a></li>
     <li><a href="<?php echo $BASE_URL; ?>/crear-pregunta">crear nueva pregunta</a></li>
+    <li><a href="<?php echo $BASE_URL; ?>/estadisticas">Ver estadisticas</a></li>
 </ul>
 </nav>
